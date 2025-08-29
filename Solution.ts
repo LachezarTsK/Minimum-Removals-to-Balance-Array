da1d291dc39465b0cd2fc9ff734d2b9fc5b36e86
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-08-29 20:41:37 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,15 @@
+
+function minRemoval(input: number[], multiplicator: number): number {
+    input.sort((x, y) => x - y);
+    let left = 0;
+    let minimumRemovals = Number.MAX_SAFE_INTEGER;
+
+    for (let right = 0; right < input.length; ++right) {
+        while (left < right && input[left] * multiplicator < input[right]) {
+            ++left;
+        }
+        minimumRemovals = Math.min(minimumRemovals, input.length - (right - left + 1));
+    }
+
+    return minimumRemovals;
+};
